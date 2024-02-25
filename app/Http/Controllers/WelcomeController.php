bd31ea81f2236b943230c0c73d5cb3a4168a165f
MuhammadIrsyadDany <irsyadnny09@gmail.com>
2024-02-26 00:02:16 +0700

    WelcomeController + helloblade

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -7,5 +7,15 @@ use Illuminate\Http\Request;
 class WelcomeController extends Controller
 {
     //
+    public function hello()
+    {
+        return 'Hello World';
+    }
 
+    public function greeting()
+    {
+        return view('blog.hello')
+            ->with('name', 'Irsyad')
+            ->with('occupation', 'Astronaut');
+    }
 }
