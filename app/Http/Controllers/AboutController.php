1f4220818e9881cae75c0d4b12122864490760b5
MuhammadIrsyadDany <irsyadnny09@gmail.com>
2024-02-26 00:00:43 +0700

    AboutController

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    //
+    public function about()
+    {
+        return '2241720227 <br> Muhammad Irsyad Dany';
+    }
+}
